c1a3a88c97b8313e006f1085690116ddf8c0715b
cpp0302 <cpp0302@gmail.com>
2014-06-12 02:25:40 +0900

    FirstName,LastName,FullNameを定義してとりあえずビルドできる状態になった

--- FirstDemo.Core/ViewModels/FirstViewModel.cs
+++ FirstDemo.Core/ViewModels/FirstViewModel.cs
@@ -2,14 +2,26 @@ using Cirrious.MvvmCross.ViewModels;
 
 namespace FirstDemo.Core.ViewModels
 {
-    public class FirstViewModel 
+	public class FirstViewModel
 		: MvxViewModel
-    {
-		private string _hello = "Hello MvvmCross";
-        public string Hello
-		{ 
-			get { return _hello; }
-			set { _hello = value; RaisePropertyChanged(() => Hello); }
+	{
+		private string _firstName;
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; RaisePropertyChanged(() => FullName); }
 		}
-    }
+
+		private string _lastName;
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; RaisePropertyChanged(() => FullName); }
+		}
+
+		public string FullName
+		{
+			get { return string.Format("{0} {1}", _firstName, _lastName); }
+		}
+	}
 }

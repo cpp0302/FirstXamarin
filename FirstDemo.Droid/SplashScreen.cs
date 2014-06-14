e493c10f261e86d91716b1ae1dc16670dd0232aa
cpp0302 <cpp0302@gmail.com>
2014-06-15 00:38:37 +0900

    SplashScreen.csを新規作成

---
+++ FirstDemo.Droid/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cirrious.MvvmCross.Droid.Views;
+
+namespace FirstDemo.Droid
+{
+	[Activity(Label = "FirstDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class SplashScreen : MvxSplashScreenActivity
+	{
+		public SplashScreen ()
+			: base(Resource.Layout.SplashScreen)
+		{
+		}
+	}
+}
+

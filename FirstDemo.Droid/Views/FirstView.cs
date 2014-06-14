21102f5689fda7bf318eb14a90f6c8f376c66062
cpp0302 <cpp0302@gmail.com>
2014-06-15 00:35:16 +0900

    内容反映

--- FirstDemo.Droid/Views/FirstView.cs
+++ FirstDemo.Droid/Views/FirstView.cs
@@ -1,35 +1,21 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Cirrious.MvvmCross.Droid.Views;
 
-namespace FirstDemo.Droid
+namespace FirstDemo.Droid.Views
 {
-	[Activity (Label = "FirstDemo.Droid", MainLauncher = true)]
-	public class MainActivity : Activity
+	[Activity(Label = "FirstDemo.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class FirstView : MvxActivity
 	{
-		int count = 1;
-
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
 		}
 	}
 }
-
-

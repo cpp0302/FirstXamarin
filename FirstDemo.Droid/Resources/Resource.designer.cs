d0fbdf4f821dbf244d2f22d60af458e23b4cace8
cpp0302 <cpp0302@gmail.com>
2014-06-15 00:01:53 +0900

    Main.axmlを削除

--- FirstDemo.Droid/Resources/Resource.designer.cs
+++ FirstDemo.Droid/Resources/Resource.designer.cs
@@ -57,46 +57,14 @@ namespace FirstDemo.Droid
 			}
 		}
 		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
-		public partial class Layout
-		{
-			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
-			
-			static Layout()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Layout()
-			{
-			}
-		}
-		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f030001
+			public const int app_name = 2130903041;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int hello = 2130903040;
 			
 			static String()
 			{

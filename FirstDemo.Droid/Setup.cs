3e1878cb8072776fa930369846e8a322ad9efe5b
cpp0302 <cpp0302@gmail.com>
2014-06-15 00:35:32 +0900

    Setup.csを新規作成

---
+++ FirstDemo.Droid/Setup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cirrious.MvvmCross.Droid.Platform;
+using Android.Content;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace FirstDemo.Droid
+{
+	public class Setup : MvxAndroidSetup
+	{
+		public Setup(Context appContext) : base(appContext) { }
+
+		protected override IMvxApplication CreateApp()
+		{
+			return new Core.App();
+		}
+	}
+}

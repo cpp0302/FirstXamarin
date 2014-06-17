4445fc53292e21188d74d7763917d4983c9e6cad
cpp0302 <cpp0302@gmail.com>
2014-06-18 01:08:47 +0900

    コア処理実装完了

--- TipCalc/TipCalc.Core/ViewModels/FirstViewModel.cs
+++ TipCalc/TipCalc.Core/ViewModels/FirstViewModel.cs
@@ -1,15 +1,54 @@
 using Cirrious.MvvmCross.ViewModels;
+using TipCalc.Core.Services;
 
 namespace TipCalc.Core.ViewModels
 {
     public class FirstViewModel 
 		: MvxViewModel
     {
-		private string _hello = "Hello MvvmCross";
-        public string Hello
-		{ 
-			get { return _hello; }
-			set { _hello = value; RaisePropertyChanged(() => Hello); }
+		private readonly ICalculationService _calculationService;
+
+		public FirstViewModel(ICalculationService calculationService)
+		{
+			_calculationService = calculationService;
+			_generosity = 20;
+			_subTotal = 100;
+			Recalc();
+		}
+
+		private double _subTotal;
+		public double SubTotal
+		{
+			get { return _subTotal; }
+			set { _subTotal = value; RaisePropertyChanged(() => SubTotal); Recalc(); }
+		}
+
+		private double _generosity;
+		public double Generosity
+		{
+			get { return _generosity; }
+			set { _generosity = value; RaisePropertyChanged(() => Generosity); Recalc(); }
+		}
+
+		private double _tip;
+		public double Tip
+		{
+			get { return _tip; }
+			set { _tip = value; RaisePropertyChanged(() => Tip); }
+		}
+
+
+		private double _total;
+		public double Total
+		{
+			get { return _total; }
+			set { _total = value; RaisePropertyChanged(() => Total); Recalc(); }
+		}
+
+		private void Recalc()
+		{
+			Tip = _calculationService.Tip(SubTotal, Generosity);
+			Total = SubTotal + Tip;
 		}
     }
 }

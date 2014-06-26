f77485457c2ec1a8ef1ba330e694eecd816ca4af
cpp0302 <cpp0302@gmail.com>
2014-06-27 02:43:51 +0900

    Listのアイテムを突っ込むレイアウトを追加。しかし実行しても画面が空になる。

--- KittenView/KittenView.Droid/Resources/Resource.Designer.cs
+++ KittenView/KittenView.Droid/Resources/Resource.Designer.cs
@@ -104,7 +104,10 @@ namespace KittenView.Droid
 			public const int FirstView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int SplashScreen = 2130903041;
+			public const int Item_Kitten = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int SplashScreen = 2130903042;
 			
 			static Layout()
 			{

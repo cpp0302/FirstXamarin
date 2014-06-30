892a7777135252988e1e71173235f2cf9697407d
cpp0302 <cpp0302@gmail.com>
2014-06-30 23:40:44 +0900

    文字を逆にするコンバーターを追加。ついでにAndroid側の画面も実装

---
+++ N-04-ValueConverters/Value.Core/Converters/StringReverseValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore.Converters;
+
+namespace Value.Core.Converters
+{
+	public class StringReverseValueConverter : MvxValueConverter<string, string>
+	{
+		protected override string Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			value = value ?? "";
+			var stringBuilder = new StringBuilder(value.Length);
+			for(var i=value.Length-1; i>=0; i--){
+				stringBuilder.Append(value[i]);
+			}
+
+			return stringBuilder.ToString();
+		}
+	}
+}

7ec03b9c8fece63ec3cb3b6748e63d6a008ad039
cpp0302 <cpp0302@gmail.com>
2014-06-30 23:58:58 +0900

    Enumを使ったConverterを作成。 0～2文字で「iOS」 3～5文字で「WindowsPhone」 6文字以上で「Android」が出てくる。もちろんEnum値がそのままテキストボックスに表示される

---
+++ N-04-ValueConverters/Value.Core/Converters/SpecialValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using Cirrious.CrossCore.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value.Core.Converters
+{
+	public enum Special
+	{
+		WindowsPhone,
+		Android,
+		iOS
+	}
+
+	public class SpecialValueConverter : MvxValueConverter<string, Special>
+	{
+		protected override Special Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			value = value ?? "";
+
+			if (value.Length < 3)
+			{
+				return Special.iOS;
+			}
+			if (value.Length < 6)
+			{
+				return Special.WindowsPhone;
+			}
+			return Special.Android;
+		}
+	}
+}

41a5cbccc0cb96991737b1d022ea024b24bf9cba
cpp0302 <cpp0302@gmail.com>
2014-06-30 23:25:13 +0900

    コンバーターを新規作成

---
+++ N-04-ValueConverters/Value.Core/Converters/StringLengthValueConverter.cs
@@ -0,0 +1,18 @@
+﻿using Cirrious.CrossCore.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value.Core.Converters
+{
+	public class StringLengthValueConverter : MvxValueConverter<string, int>
+	{
+		protected override int Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			value = value ?? "";
+			return value.Length;
+		}
+	}
+}

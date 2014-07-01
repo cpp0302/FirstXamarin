68f114d7242d25e394db0c603ff78bbdcaeff2d1
cpp0302 <cpp0302@gmail.com>
2014-07-02 01:00:52 +0900

    iPhoneでも3つ目の画面を作成

--- N-05-MultiPage/MultiPage.Touch/Views/ThirdView.cs
+++ N-05-MultiPage/MultiPage.Touch/Views/ThirdView.cs
@@ -7,12 +7,12 @@ using MonoTouch.Foundation;
 
 namespace MultiPage.Touch.Views
 {
-    [Register("SecondView")]
-    public class SecondView : MvxViewController
+    [Register("ThirdView")]
+    public class ThirdView : MvxViewController
     {
         public override void ViewDidLoad()
         {
-            View = new UIView(){ BackgroundColor = UIColor.Red};
+            View = new UIView(){ BackgroundColor = UIColor.Cyan};
             base.ViewDidLoad();
 
 			// ios7 layout
@@ -21,14 +21,9 @@ namespace MultiPage.Touch.Views
 			   
             var label = new UILabel(new RectangleF(10, 10, 300, 40));
 			Add(label);
-			var button1 = new UIButton(UIButtonType.RoundedRect);
-			button1.SetTitle("Go Third", UIControlState.Normal);
-			button1.Frame = new RectangleF(10, 90, 300, 40);
-			Add(button1);
 
-			var set = this.CreateBindingSet<SecondView, Core.ViewModels.SecondViewModel>();
-			set.Bind(label).To(vm => vm.Name);
-			set.Bind(button1).To(vm => vm.GoThirdCommand);
+            var set = this.CreateBindingSet<ThirdView, Core.ViewModels.ThirdViewModel>();
+            set.Bind(label).To(vm => vm.TheAnswer);
             set.Apply();
         }
     }

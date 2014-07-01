a286e1fd1141607b26288bd12a0a6725bde0b774
cpp0302 <cpp0302@gmail.com>
2014-07-02 00:56:43 +0900

    Androidに3つ目の画面と2つ目から3つ目の画面肉ためのボタンを追加

--- N-05-MultiPage/MultiPage.Droid/Resources/Resource.Designer.cs
+++ N-05-MultiPage/MultiPage.Droid/Resources/Resource.Designer.cs
@@ -109,6 +109,9 @@ namespace MultiPage.Droid
 			// aapt resource value: 0x7f030002
 			public const int SplashScreen = 2130903042;
 			
+			// aapt resource value: 0x7f030003
+			public const int ThirdView = 2130903043;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();

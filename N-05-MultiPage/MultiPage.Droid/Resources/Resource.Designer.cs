07f39fad4ba9f743da8c9cc69f2d4ba32f8ab0ef
cpp0302 <cpp0302@gmail.com>
2014-07-02 00:33:13 +0900

    最初の画面に2つ目の画面へのボタンを追加 2つ目の画面を新規作成 やっぱりSecondView.csの SetContentView メソッドでどのViewModelを使うか決めているみたい

--- N-05-MultiPage/MultiPage.Droid/Resources/Resource.Designer.cs
+++ N-05-MultiPage/MultiPage.Droid/Resources/Resource.Designer.cs
@@ -104,7 +104,10 @@ namespace MultiPage.Droid
 			public const int FirstView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int SplashScreen = 2130903041;
+			public const int SecondView = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int SplashScreen = 2130903042;
 			
 			static Layout()
 			{

94d1553bd7679d20903f2bc49f806af8156fd4b9
cpp0302 <cpp0302@gmail.com>
2014-07-02 00:02:22 +0900

    変数Helloに " World" を足すコマンドを追加

--- N-05-MultiPage/MultiPage.Core/ViewModels/FirstViewModel.cs
+++ N-05-MultiPage/MultiPage.Core/ViewModels/FirstViewModel.cs
@@ -2,14 +2,31 @@ using Cirrious.MvvmCross.ViewModels;
 
 namespace MultiPage.Core.ViewModels
 {
-    public class FirstViewModel 
+	public class FirstViewModel
 		: MvxViewModel
-    {
+	{
 		private string _hello = "Hello MvvmCross";
-        public string Hello
-		{ 
+		public string Hello
+		{
 			get { return _hello; }
 			set { _hello = value; RaisePropertyChanged(() => Hello); }
 		}
-    }
+
+
+		MvxCommand _myCommand;
+		public System.Windows.Input.ICommand MyCommand
+		{
+			get
+			{
+				_myCommand = _myCommand ?? new MvxCommand(DoMyCommand);
+				return _myCommand;
+			}
+		}
+
+		void DoMyCommand()
+		{
+			this.Hello = this.Hello + " World";
+		}
+
+	}
 }
